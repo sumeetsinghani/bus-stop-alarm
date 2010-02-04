1a3fc9e3381370045fc0d9f3fb48ba25cae76e77
byonil0222 <byonil0222@ea6106b8-ffb0-11de-bcdd-59c7212cd493>
2010-02-04 21:46:58 +0000

    we added vibrate and ringtone in the confirmation page. since the android emulator does not support vibration, it pops up an error message when you click on vibrate. and it does not have any ringtones in the sd card. we just tried to get the attributes of the ringtones and put them on the list.

--- src/com/busstopalarm/ConfirmationPage.java
+++ src/com/busstopalarm/ConfirmationPage.java
@@ -1,17 +1,22 @@
 package com.busstopalarm;
 
-import com.busstopalarm.R;
-
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.media.RingtoneManager;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
 public class ConfirmationPage extends Activity {
 	/** Called when the activity is first created. */
@@ -23,6 +28,9 @@ public class ConfirmationPage extends Activity {
 		String stop = getIntent().getStringExtra("name");
 		TextView stopView = (TextView) findViewById(R.id.stopname);
 		stopView.setText(stop);
+		
+	
+		
 		
 		final Button OKButton = (Button)findViewById(R.id.OKButton);
 		OKButton.setOnClickListener(new View.OnClickListener() {
@@ -39,11 +47,47 @@ public class ConfirmationPage extends Activity {
 			}	
 		});
 		
+		RingtoneManager ringtoneManager = new RingtoneManager(this);
+		ringtoneManager.setType(RingtoneManager.TYPE_RINGTONE);  // only to get ringtone type 
+		Cursor ringtoneCursor = ringtoneManager.getCursor();
+		String[] ringtoneList = ringtoneCursor.getColumnNames();
+		
+		Spinner ringtoneSpinner = (Spinner) findViewById(R.id.RingtoneSelector);
+		ArrayAdapter<String> ringtoneAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, ringtoneList);
+		ringtoneAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		ringtoneSpinner.setAdapter(ringtoneAdapter);
+	
 		Spinner proximityUnits = (Spinner) findViewById(R.id.ProximityUnits);
 		ArrayAdapter<CharSequence> proxSpinnerValues = ArrayAdapter.createFromResource(this, R.array.ProximityUnitList, android.R.layout.simple_spinner_item);
 		proxSpinnerValues.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		proximityUnits.setAdapter(proxSpinnerValues);
+		
+		
+		// vibrate check box
+		final CheckBox vib = (CheckBox) findViewById(R.id.VibrateCheckbox);
+		final Vibrator vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+		vib.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				// TODO Auto-generated method stub
+						
+				if (isChecked) { // vibrate is checked
+					//	vib.setChecked(false);    
+			       //buttonView.setBackgroundResource(R.drawable.black);
+				    vibrator.vibrate(5000);  // 5000 milliseconds = 5 seconds
+				}
+				if (!isChecked) {
+				  //  buttonView.setBackgroundColor(R.drawable.icon);
+                	vibrator.cancel();
+				}	
+					
+			}
+			});
+		
+		
 	}
+
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
