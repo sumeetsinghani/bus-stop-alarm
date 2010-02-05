7a4bbad5704a3addcb319159895ab01c7e103546
byonil0222 <byonil0222@ea6106b8-ffb0-11de-bcdd-59c7212cd493>
2010-02-05 22:42:42 +0000

    ok, we added the ringtone list (if you run the emulator, you won't see the list of ringtones because the emulator does not have any ringtones. So there will be nothing there.)

--- trunk/src/com/busstopalarm/ConfirmationPage.java
+++ trunk/src/com/busstopalarm/ConfirmationPage.java
@@ -7,6 +7,7 @@ import android.database.Cursor;
 import android.media.RingtoneManager;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,7 +19,12 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 
+
 public class ConfirmationPage extends Activity {
+	
+// this TAG is for debugging
+	private static final String TAG = "toViewLogs";
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -30,8 +36,6 @@ public class ConfirmationPage extends Activity {
 		stopView.setText(stop);
 		
 	
-		
-		
 		final Button OKButton = (Button)findViewById(R.id.OKButton);
 		OKButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
@@ -47,15 +51,39 @@ public class ConfirmationPage extends Activity {
 			}	
 		});
 		
+		
+		/*
+		Intent intentAlarm = new Intent(this, RepeatingAlarmReceiver.class);
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(context, REQUEST_CODE, intentAlarm, 0);
+
+		AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+		alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + (5 * 1000), 10 * 1000, pendingIntent);
+		Toast.makeText(this, "Alarm set", Toast.LENGTH_LONG).show();
+		*/
+		
+		
+		// ringtone
 		RingtoneManager ringtoneManager = new RingtoneManager(this);
 		ringtoneManager.setType(RingtoneManager.TYPE_RINGTONE);  // only to get ringtone type 
-		Cursor ringtoneCursor = ringtoneManager.getCursor();
-		String[] ringtoneList = ringtoneCursor.getColumnNames();
 		
+		//Ringtone ringtone = ringtoneManager.getRingtone(0);
+		Cursor ringtoneCursor = ringtoneManager.getCursor();
+		Log.v(TAG, "column count: " + ringtoneCursor.getColumnCount());
+		
+		String[] ringtoneList = new String[ringtoneCursor.getCount()];
+		Log.v(TAG, "row count: " + ringtoneCursor.getCount());
+		for (int i = 0; i < ringtoneCursor.getCount(); i++)
+		  ringtoneList[i] = ringtoneCursor.getString(ringtoneManager.TITLE_COLUMN_INDEX);
+	
+		//String ringtoneList = ringtone.getTitle(this);	
+		// = ringtoneCursor.getColumnNames();
+
 		Spinner ringtoneSpinner = (Spinner) findViewById(R.id.RingtoneSelector);
 		ArrayAdapter<String> ringtoneAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, ringtoneList);
+		//ringtoneAdapter.add(ringtoneCursor.getColumnCount() + "");
 		ringtoneAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		ringtoneSpinner.setAdapter(ringtoneAdapter);
+	
 	
 		Spinner proximityUnits = (Spinner) findViewById(R.id.ProximityUnits);
 		ArrayAdapter<CharSequence> proxSpinnerValues = ArrayAdapter.createFromResource(this, R.array.ProximityUnitList, android.R.layout.simple_spinner_item);
