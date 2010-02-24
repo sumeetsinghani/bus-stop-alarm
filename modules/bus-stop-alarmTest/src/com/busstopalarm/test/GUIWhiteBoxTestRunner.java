af44f4eea092a647b1ea1142ab004ef9d4e5026f
orkhan89 <orkhan89@ea6106b8-ffb0-11de-bcdd-59c7212cd493>
2010-02-24 06:21:57 +0000

    Added White box testing for GUI MainPageTests and ConfirmationPageTests

---
+++ modules/bus-stop-alarmTest/src/com/busstopalarm/test/GUIWhiteBoxTestRunner.java
@@ -0,0 +1,27 @@
+package com.busstopalarm.test;
+
+import junit.framework.TestSuite;
+import android.test.InstrumentationTestRunner;
+import android.test.InstrumentationTestSuite;
+import android.util.Log;
+
+
+public class GUIWhiteBoxTestRunner extends InstrumentationTestRunner{
+	
+	 @Override
+	    public TestSuite getAllTests() {
+	        InstrumentationTestSuite suite = new InstrumentationTestSuite(this);
+	        suite.addTestSuite(WhiteBoxGUIMainPageTests.class);
+	        suite.addTestSuite(WhiteBoxGUIConfirmationPageTests.class);
+	        return suite;
+	    }
+
+	    @Override
+	    public ClassLoader getLoader() {
+	        return GUIWhiteBoxTestRunner.class.getClassLoader();
+	    }
+	    
+	
+
+
+}
