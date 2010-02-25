6b5e75137b0755a267ec978bae072348858ad8ba
orkhan89 <orkhan89@ea6106b8-ffb0-11de-bcdd-59c7212cd493>
2010-02-25 05:06:36 +0000

    Fixed OK button and manifest for HelpPage.java

--- trunk/src/com/busstopalarm/HelpPage.java
+++ trunk/src/com/busstopalarm/HelpPage.java
@@ -8,12 +8,15 @@ package com.busstopalarm;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
 /**
- * @author David
+ * @author David Truong, Orkhan Muradov
  *
  */
 public class HelpPage extends Activity {
@@ -22,5 +25,12 @@ public class HelpPage extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.help);
+		final Button OK = (Button)findViewById(R.id.btnOk);
+		OK.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Log.d("OK", "OK IN HELP MENU IS PUSHED");
+				finish();
+			}	
+		});
 	}
 }
