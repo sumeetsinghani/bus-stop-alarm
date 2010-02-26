77311d900b7dfb2fad94e8d00d2d0c4931a1bc61
mneng7 <mneng7@ea6106b8-ffb0-11de-bcdd-59c7212cd493>
2010-02-26 14:01:00 +0000

    Added "Test Driven" JUnit Test File DataFetcherTest.java to src folder to test DataFetcher.java Modified/Added/Commented DataFetcher.java headers. Added TestStrings.xml into resource folder to hold the long expected values from OneBusAway API call. Add Stop.java stub class for future use. Modified MapPage.java because DataFetcher.getRouteById() no longer returns a string.

--- src/com/busstopalarm/MainPage.java
+++ src/com/busstopalarm/MainPage.java
@@ -1,6 +1,5 @@
 package com.busstopalarm;
 
-
 import java.io.FileNotFoundException;
 
 import java.io.IOException;
@@ -21,9 +20,10 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
 /**
  * Main page of the Bus Stop Alarm
- *
+ * 
  */
 public class MainPage extends Activity {
 	/** Called when the activity is first created. */
@@ -31,76 +31,78 @@ public class MainPage extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		final Button RouteSearchButton = (Button)findViewById(R.id.RouteSearchButton);
-		
+		final Button RouteSearchButton = (Button) findViewById(R.id.RouteSearchButton);
+
 		// search button behavior
 		RouteSearchButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				String routeText = ((EditText)findViewById(R.id.RouteSearchBox)).getText().toString();
-				try {
-					if (routeText.matches("[0-9]{1,5}")) {
-						int routeNumber = Integer.parseInt(routeText);
-						String s = DataFetcher.getRouteById(routeNumber);
-						startActivity(new Intent(v.getContext(), MapPage.class));
-						Toast t = Toast.makeText(v.getContext(), s.subSequence(0, 500), Toast.LENGTH_LONG);
-						t.show();
-					} else {
-						Toast t = Toast.makeText(v.getContext(), "Invalid Route Number", Toast.LENGTH_LONG);
-						t.show();
-					}
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+				String routeText = ((EditText) findViewById(R.id.RouteSearchBox))
+						.getText().toString();
+				if (routeText.matches("[0-9]{1,5}")) {
+					int routeNumber = Integer.parseInt(routeText);
+					// String s = DataFetcher.getRouteById(routeNumber);
+					// //DataFetcher implementation changed.
+					String s = "PlaceHolder for Route Info.";
+					startActivity(new Intent(v.getContext(), MapPage.class));
+					Toast t = Toast.makeText(v.getContext(), s.subSequence(0,
+							Math.max(s.length(), 500)), Toast.LENGTH_LONG);
+					t.show();
+				} else {
+					Toast t = Toast.makeText(v.getContext(),
+							"Invalid Route Number", Toast.LENGTH_LONG);
+					t.show();
 				}
-			}	
+			}
 		});
-		
+
 		// favorite button behavior
-		final Button FavButton = (Button)findViewById(R.id.FavButton);
+		final Button FavButton = (Button) findViewById(R.id.FavButton);
 		FavButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				Intent i = new Intent(v.getContext(), LocationListPage.class);
 				i.putExtra("listType", LocationListPage.FAVORITES);
 				startActivity(i);
 				finish();
-			}	
+			}
 		});
-		
+
 		// major button behavior
-		final Button MajorButton = (Button)findViewById(R.id.MajorButton);
+		final Button MajorButton = (Button) findViewById(R.id.MajorButton);
 		MajorButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				Intent i = new Intent(v.getContext(), LocationListPage.class);
 				i.putExtra("listType", LocationListPage.MAJOR);
 				startActivity(i);
 				finish();
-			}	
+			}
 		});
-		
+
 		// TODO: add recent routes to main page
 		BusDbAdapter ad = new BusDbAdapter(getApplicationContext());
 		ad.open();
 		Cursor recent = ad.getRecentDest(5);
-		LinearLayout recentList = (LinearLayout)findViewById(R.id.recent_routes);
+		LinearLayout recentList = (LinearLayout) findViewById(R.id.recent_routes);
 		int routeIndex = recent.getColumnIndex("route_id");
 		int routeDescIndex = recent.getColumnIndex("route_desc");
 		while (!recent.isAfterLast()) {
 			final TextView recentItem = new TextView(this);
 			recentItem.setClickable(true);
-			
+
 			recentItem.setOnClickListener(new View.OnClickListener() {
 				public void onClick(View v) {
-					Toast t = Toast.makeText(getApplicationContext(), recentItem.getText(), Toast.LENGTH_SHORT);
+					Toast t = Toast.makeText(getApplicationContext(),
+							recentItem.getText(), Toast.LENGTH_SHORT);
 					t.show();
-				}	
+				}
 			});
-			
+
 			recentItem.setTextSize(TypedValue.COMPLEX_UNIT_PT, 10);
-			recentItem.setText("Route " + recent.getString(routeIndex) + ", " + recent.getString(routeDescIndex));
+			recentItem.setText("Route " + recent.getString(routeIndex) + ", "
+					+ recent.getString(routeDescIndex));
 			recentList.addView(recentItem);
 			recent.moveToNext();
 		}
-		
+
 	}
 
 	@Override
@@ -109,10 +111,9 @@ public class MainPage extends Activity {
 		menu.add(0, 1, 1, "Confirmation");
 		menu.add(0, 2, 2, "Help/About");
 		menu.add(0, 3, 3, "Num3");
-		menu.add(0,4,4, "Exit");
+		menu.add(0, 4, 4, "Exit");
 		return true;
 	}
-
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
