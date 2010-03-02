18a9b3417a507567ee93fa4842cedf02a67c78c6
orkhan89 <orkhan89@ea6106b8-ffb0-11de-bcdd-59c7212cd493>
2010-03-02 03:45:02 +0000

    more commenting on OverlayHelper

--- trunk/src/com/busstopalarm/ItemizedOverlayHelper.java
+++ trunk/src/com/busstopalarm/ItemizedOverlayHelper.java
@@ -29,7 +29,7 @@ public class ItemizedOverlayHelper extends ItemizedOverlay {
 	
 
 	/**
-	 * 
+	 * It shows current bus route and a button to set alarm.
 	 * @param mCtx gets activity
 	 * @param defaultMarker grabs marker
 	 */
