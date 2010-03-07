0f1a657b6c2b362b620c2a34cad319b034537f2d
byonil0222 <byonil0222@ea6106b8-ffb0-11de-bcdd-59c7212cd493>
2010-03-07 18:50:07 +0000

    deleted some stuffs not used in confirmation page and added the case when proximityUnit is Minutes in alarm service class

--- trunk/src/com/busstopalarm/AlarmService.java
+++ trunk/src/com/busstopalarm/AlarmService.java
@@ -23,30 +23,31 @@ import android.util.Log;
  *
  */
 public class AlarmService extends Service {
-	
+
 	private static final int NOTIFICATION_ID1 = 1001;
 	private static final int PENDING_INTENT_REQUEST_CODE1 = 1000001;
 	private static final int PENDING_INTENT_REQUEST_CODE2 = 1000002;
 	private static final int MIN_TIME_TO_UPDATE_LOCATION = 5000; // in milliseconds
 	private static final int MIN_DISTANCE_TO_UPDATE_LOCATION = 5; // in meters
-	
+
 	private LocationManager lm;
 	private Location currentLoc;
 	private NotificationManager mNtf;
 	private Notification ntf;
-	
+
 	private int proximity;
 	private String proximityUnit;
 	private BusStop busStop;
-	
+	private AlarmManager alarmManager;
+
 	/**
 	 * setup service managers.
 	 */
 	public void onCreate() {
 		super.onCreate();
-		
+
 		lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-		
+		alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 		/*
 		 * TODO: by setting minTime and minDistance both to 0, the battery will
 		 * drain out really fast.!!!! Set the approriate value here.
@@ -56,7 +57,64 @@ public class AlarmService extends Service {
 		mNtf = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 		ntf = new Notification(R.drawable.busstopicon, "Alarm Set!", System.currentTimeMillis());
 	}
-	
+
+
+
+	/**
+	 *  For distance between two points, we will use Euclidean distance.
+	 *  The Earth is not an Euclidean plane,
+	 *  but this will give a good approximation. Assuming Euclidean plane, 
+	 *  this algorithm sums a number of straight line distances.  This means the
+	 *  calculated distance will never underestimate the actual distance, which
+	 *  is good.
+	 *  To calculate the remaining distance once alarm has started, we need to 
+	 *  get the current location with the GPS.
+	 *  Then we need to find the closest busstop to the current location 
+	 *  (with caveat), then do sum of straight lines again.
+	 *  The return values will be in some unit that will need to be converted 
+	 *  to either miles or km. 
+	 *  not implemented yet!
+	 *   
+	 * @return double initial distance
+	 */ 
+	public static double calculateInitialDistance() {
+		// get starting s busstop in busroute
+		// will the starting busstop be specified by the user or does the app
+		// have to figure it out?
+		// get ending d busstop in busroute
+		// double dist = 0.0;
+		// for (int i = s; i < d; i++) {
+		//     dist += calculateDist(busroute[i],busroute[i+1];
+		// return dist;
+		return 0.0;
+	}
+
+
+	/**
+	 * not implemented yet!
+	 * 
+	 * @return double remaining distance
+	 */
+	public static double calculateRemainingDistance() {
+		return 0.0;
+	}
+
+
+	/**
+	 *  Updates the average speed based on previous average speed and current
+	 *  speed. If implemented like this, we need a average speed field?
+	 *  We could start with an initial average speed (equivalent to 30 mph?) 
+	 *  and do a something like
+	 *  avg = k*avg + (1-k)current where 0 <= k <= 1.
+	 *  not implemented yet!
+	 *  
+	 */
+	public static void updateAverageSpeed() {
+
+	}
+
+
+
 	/**
 	 * when "Cancel" button is pressed on the confirmation page, this method
 	 * will be called and cancel the current alarm set and the notification.
@@ -71,8 +129,8 @@ public class AlarmService extends Service {
 		pendingIntentAlarm.cancel();
 		Log.d("ALARMSERVICE", "current alarm is destroyed");
 	}
-	
-	
+
+
 	/**
 	 * When the service is started, it starts listening to the GPS.
 	 */
@@ -82,19 +140,19 @@ public class AlarmService extends Service {
 		busStop = intent.getParcelableExtra("busstop");
 		Uri ringtoneUri = intent.getParcelableExtra("ringtoneUri");
 		boolean vibration = intent.getBooleanExtra("vibration", false);
-		
+
 		PendingIntent pi = PendingIntent.getBroadcast(
 				getApplicationContext(), PENDING_INTENT_REQUEST_CODE2,
 				new Intent(getApplicationContext(), AlarmService.class), 
 				PendingIntent.FLAG_UPDATE_CURRENT);
 		ntf.setLatestEventInfo(getApplicationContext(), "Bus Stop: " + busStop.getName(),
 				"acquiring location...", pi);
-		
+
 		mNtf.notify(NOTIFICATION_ID1, ntf);
 
 		Log.d("ALARMSERVICE", "prox: " + proximity + ", units: " + proximityUnit + ", stop: " + busStop +
 				", ringtone: " + ringtoneUri + ", vibration: " + vibration);
-		
+
 		Intent alarmIntent = new Intent(getApplicationContext(), OneTimeAlarmReceiver.class);
 
 		alarmIntent.putExtra("ringtoneUri", ringtoneUri);
@@ -103,20 +161,26 @@ public class AlarmService extends Service {
 				PENDING_INTENT_REQUEST_CODE1, alarmIntent,
 				PendingIntent.FLAG_CANCEL_CURRENT);
 		float proximityInput = (float) proximity;
-		if (proximityUnit.equals("Yards"))
-			proximityInput = convertYardsToMeters(proximityInput);
-		lm.addProximityAlert(busStop.getLatitude(), busStop.getLongitude(),
-				proximityInput, -1, pendingIntentAlarm);
-	}
+		if (proximityUnit.equals("Minutes")) {
+			//	alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() +
+			//			((time remaining - proximity(in minutes)) (in seconds) * 1000), pendingIntentAlarm);
+		} else { // Yards or Meters
+			if (proximityUnit.equals("Yards"))
+				proximityInput = convertYardsToMeters(proximityInput);
 	
+			lm.addProximityAlert(busStop.getLatitude(), busStop.getLongitude(),
+					proximityInput, -1, pendingIntentAlarm);
+		}
+	}
+
 	public float convertYardsToMeters(float yards){
 		return (float) (yards * 0.9144);
 	}
-	
+
 	public float convertMetersToYards(float meters){
 		return (float) (meters * 1.0936133);
 	}
-	
+
 	/**
 	 * Service requires that this function must be overridden, but we don't need it.
 	 */
@@ -125,7 +189,7 @@ public class AlarmService extends Service {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	/**
 	 * {@link AlarmLocationListener} listens to the GPS, and updates the users location
 	 * 
@@ -133,7 +197,7 @@ public class AlarmService extends Service {
 	 *
 	 */
 	private class AlarmLocationListener implements LocationListener {
-		
+
 		/**
 		 * Updates the current Location and notification.
 		 */
@@ -145,31 +209,31 @@ public class AlarmService extends Service {
 			float dist = currentLoc.distanceTo(target);  // in meters
 			if (proximityUnit.equals("Yards"))
 				dist = convertMetersToYards(dist);
-			
+
 			PendingIntent pi = PendingIntent.getBroadcast(
 					getApplicationContext(), 0, new Intent(getApplicationContext(), AlarmService.class), 
 					PendingIntent.FLAG_UPDATE_CURRENT);
 			ntf.setLatestEventInfo(getApplicationContext(), "Bus Stop: " + busStop.getName(),
 					dist + " " + proximityUnit + " away", pi); // TODO: convert to correct units
-			
+
 			ntf.when = System.currentTimeMillis();
 			mNtf.notify(NOTIFICATION_ID1, ntf);
 			Log.d("ALARMSERVICE", "location updated");
 		}
-	
+
 		public void onProviderDisabled(String provider) {
 			// TODO Auto-generated method stub
-			
+
 		}
-	
+
 		public void onProviderEnabled(String provider) {
 			// TODO Auto-generated method stub
-			
+
 		}
-	
+
 		public void onStatusChanged(String provider, int status, Bundle extras) {
 			// TODO Auto-generated method stub
-			
+
 		}
 	}
 
