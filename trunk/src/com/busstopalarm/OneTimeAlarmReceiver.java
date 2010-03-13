913058248b2ea2c9d539cd3cc6f752bb756c9b5b
byonil0222 <byonil0222@ea6106b8-ffb0-11de-bcdd-59c7212cd493>
2010-03-13 18:56:52 +0000

    cleaning up, and commenting on OneTimeAlarmReceiver class

--- trunk/src/com/busstopalarm/OneTimeAlarmReceiver.java
+++ trunk/src/com/busstopalarm/OneTimeAlarmReceiver.java
@@ -6,8 +6,7 @@
  * by notifying with the selected ringtone / vibrate when it receives the signal
  * from the system. It wakes up the phone if the phone is sleeping.
  * If given ringotne is null, it does not ring.
- * If given vibrate is null, it does not vibrate
- * TODO: alarm update consistently
+ * If given vibrate is null, it does not vibrate.
  * 		    
  */
 
@@ -29,22 +28,28 @@ public class OneTimeAlarmReceiver extends BroadcastReceiver {
 	private static final String TAG = "inOneTimeAlarmReceiver";
 
 	private static final int NOTIFICATION_ID2 = 1002;
-	//private static final int PENDING_INTENT_REQUEST_CODE3 = 1000003;
 
- // this variable is for testing
+ // this variable is for testing whether it successfully goes through
+ // the method onCreate and alarm goes off.
 	private boolean ifSuccessful;
 	
+	
+	/**
+	 * OneTimeAlarmReceiver constructor 
+	 * initializes the field ifSuccessful to false
+	 * since it has not gone through the method onCreate and succeeded
+	 * alarming yet.
+	 */
 	public OneTimeAlarmReceiver(){
 		ifSuccessful = false; 
 	}
 	
 	
-	
 	/**
 	 * This is called when the BroadcastReceiver receives the signal and the 
-	 * intent from the system.
-	 * It alerts the user with the notification (possibly with ringtone and 
-	 * vibrate) and with the Toast message.
+	 * intent from the system (when the device gets within the defined
+	 * proximity. It alerts the user with the notification (possibly with the
+	 * user-defined ringtone and vibrate) and toasts a message.
 	 * If it successfully goes through this method, then
 	 * it will set ifSuccessful to true (for testing purpose).
 	 * @param context in which the work is being done
@@ -82,15 +87,14 @@ public class OneTimeAlarmReceiver extends BroadcastReceiver {
 		Toast.makeText(context, "Hey Wake up! (Alarm is ringing now!)",
 				Toast.LENGTH_LONG).show();
 		
-		
 		context.stopService(new Intent(context, AlarmService.class));
 		ifSuccessful = true;
 	}
 	
 	
 	/**
-	 * getter for ifSuccessful
-	 * @return boolean 
+	 * getter for the field ifSuccessful.
+	 * @return boolean (true if alarm successfully goes off) 
 	 */
 	public boolean getIfSuccessful() {
 		return ifSuccessful;
