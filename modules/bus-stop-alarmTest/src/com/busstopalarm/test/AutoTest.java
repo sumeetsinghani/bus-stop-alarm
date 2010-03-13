75f666126b4b57f1d9d9d1ceef3894a4afc8a9da
orkhan89 <orkhan89@ea6106b8-ffb0-11de-bcdd-59c7212cd493>
2010-03-13 03:33:14 +0000

    autotest version 2

--- modules/bus-stop-alarmTest/src/com/busstopalarm/test/AutoTest.java
+++ modules/bus-stop-alarmTest/src/com/busstopalarm/test/AutoTest.java
@@ -1,12 +1,21 @@
 package com.busstopalarm.test;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import android.content.Intent;
 import android.database.Cursor;
 import android.test.ActivityInstrumentationTestCase2;
 import com.busstopalarm.BusRoute;
 import com.busstopalarm.BusStop;
 import com.busstopalarm.DataFetcher;
+
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -14,13 +23,15 @@ import android.widget.TextView;
 import com.busstopalarm.BusRoute;
 import com.busstopalarm.BusStop;
 import com.busstopalarm.ConfirmationPage;
+import com.busstopalarm.ItemizedOverlayHelper;
 import com.busstopalarm.MainPage;
 import com.busstopalarm.MapPage;
+import com.google.android.maps.GeoPoint;
 import com.jayway.android.robotium.solo.Solo;
 
 public class AutoTest extends ActivityInstrumentationTestCase2<MainPage> {
 
-	DataFetcher fetcher = new DataFetcher();
+	DataFetcher df = new DataFetcher();
 
 
 	public AutoTest() {
@@ -29,7 +40,7 @@ public class AutoTest extends ActivityInstrumentationTestCase2<MainPage> {
 
 	protected void setUp() throws Exception {
 	}
-
+/*
 	//goes to help page then to main page then settings page
 	public void test1() throws Throwable{
 		Solo solo = new Solo(getInstrumentation(), getActivity());
@@ -39,11 +50,29 @@ public class AutoTest extends ActivityInstrumentationTestCase2<MainPage> {
 		//go to #72 route
 		solo.clickOnButton("Go");
 		MapPage mp = (MapPage) solo.getCurrentActivity();
-		solo.clickOnImage(3);
-		
+		//Thread.sleep(20000);
+		//solo.clickOnImage(3);
+		String routeID = mp.getIntent().getStringExtra("routeID");
+		BusRoute br = df.getBusRouteById(Integer.parseInt(routeID.split("_")[1]), true);
+		List<BusStop> bsArr = br.getBusStops();
+		int rand = (int) ((Math.random() * 100)% bsArr.size());
+		BusStop bs = bsArr.get(rand);
+		GeoPoint point = bs.getGeoPoint();
+		ArrayList<ImageView> images = solo.getCurrentImageViews();
+		solo.clickOnImage(5);
+		mp.mapOverlays.listIterator().next().onTap(point, mp.mapView);
+		solo.clickOnButton("Set Alarm");
+		//ItemizedOverlayHelper help = new ItemizedOverlayHelper(mp, );
 		//press on a busstop
-		mp.getCurrentFocus().focusSearch(1);
-		mp.getCurrentFocus().performClick();
+		//mp.getCurrentFocus().focusSearch(1);
+		//mp.getCurrentFocus().performClick();
+		
+		/*BusRoute br = new BusRoute();
+		List<BusStop> bsArr = br.getBusStops();
+		int rand = (int) ((Math.random() * 100)% bsArr.size());
+		BusStop bs = bsArr.get(rand);
+		GeoPoint point = bs.getGeoPoint();
+		
 		
 		BusStop expected = new BusStop();
 		expected.setStopId("1_9990");
@@ -59,93 +88,102 @@ public class AutoTest extends ActivityInstrumentationTestCase2<MainPage> {
 
 	}
 
-//	//this test goes to settings page and tests if settings were saved
-//	public void test2() throws Throwable{
-//		Solo solo = new Solo(getInstrumentation(), getActivity());
-//		MainPage mp = (MainPage) getActivity();
-//		
-//		//click on Settings in menu
-//		solo.pressMenuItem(3);
-//		ConfirmationPage cp = (ConfirmationPage) solo.getCurrentActivity();
-//		
-//		//check vibrate	box
-//		final CheckBox checkBox= (CheckBox)
-//		cp.findViewById(com.busstopalarm.R.id.VibrateCheckbox);
-//		runTestOnUiThread(new Runnable() {
-//			public void run() {
-//				checkBox.performClick();
-//			}
-//		});
-//		boolean VibCheck = checkBox.isChecked();
-//		
-//		//change ringtone
-//		cp.loadRecentSettings();
-//		final Spinner rSelect = (Spinner) 
-//		cp.findViewById(com.busstopalarm.R.id.RingtoneSelector);
-//		runTestOnUiThread(new Runnable() {
-//			public void run() {
-//				rSelect.setSelection(2);
-//			}
-//		});
-//		String oldRingtone = rSelect.getSelectedItem().toString();
-//		
-//		//change proximity
-//		final Spinner PSelect = (Spinner) 
-//		cp.findViewById(com.busstopalarm.R.id.ProximityUnits);
-//		runTestOnUiThread(new Runnable() {
-//			public void run() {
-//				PSelect.setSelection(1);
-//			}
-//		});
-//		int oldProximity = Integer.parseInt(rSelect.getSelectedItem().toString());
-//		
-//		//change units
-//		final SeekBar proxBar = (SeekBar) 
-//		cp.findViewById(com.busstopalarm.R.id.ProximityBar);
-//		runTestOnUiThread(new Runnable() {
-//			public void run() {
-//				proxBar.incrementProgressBy(4);
-//			}
-//		});
-//		int OldProxUnits = proxBar.getProgress();
-//		
-//		//save settings as exit
-//		solo.clickOnButton("Save as favorite");
-//		cp.finish();
-//		
-//		//load settings
-//		cp = (ConfirmationPage) solo.getCurrentActivity();
-//		cp.loadRecentSettings();
-//		boolean vibEquals =  (VibCheck == cp.vibration);
-//		boolean ringEquals = (oldRingtone.equals(cp.dataRingtone));  
-//		boolean proxiEquals = (oldProximity == cp.proximity);
-//		boolean proxUnits = (OldProxUnits == Integer.parseInt(cp.proximityUnit));
-//	
-//		//check if settings were loaded correctly
-//		assertTrue(vibEquals && ringEquals && proxiEquals && proxUnits);
-//	}
+	//this test goes to settings page and tests if settings were saved
+	public void test2() throws Throwable{
+		Solo solo = new Solo(getInstrumentation(), getActivity());
+		MainPage mp = (MainPage) getActivity();
+		
+		//click on Settings in menu
+		solo.pressMenuItem(3);
+		ConfirmationPage cp = (ConfirmationPage) solo.getCurrentActivity();
+		
+		//check vibrate	box
+		final CheckBox checkBox= (CheckBox)
+		cp.findViewById(com.busstopalarm.R.id.VibrateCheckbox);
+		runTestOnUiThread(new Runnable() {
+			public void run() {
+				checkBox.performClick();
+			}
+		});
+		boolean VibCheck = checkBox.isChecked();
+		
+		//change ringtone
+		//cp.loadRecentSettings();
+		final Spinner rSelect = (Spinner) 
+		cp.findViewById(com.busstopalarm.R.id.RingtoneSelector);
+		runTestOnUiThread(new Runnable() {
+			public void run() {
+				rSelect.setSelection(2);
+			}
+		});
+		String oldRingtone = rSelect.getSelectedItem().toString();
+		
+		//change proximity
+		final Spinner PSelect = (Spinner) 
+		cp.findViewById(com.busstopalarm.R.id.ProximityUnits);
+		runTestOnUiThread(new Runnable() {
+			public void run() {
+				PSelect.setSelection(1);
+			}
+		});
+		int oldProximity = Integer.parseInt(rSelect.getSelectedItem().toString());
+		
+		//change units
+		final SeekBar proxBar = (SeekBar) 
+		cp.findViewById(com.busstopalarm.R.id.ProximityBar);
+		runTestOnUiThread(new Runnable() {
+			public void run() {
+				proxBar.incrementProgressBy(4);
+			}
+		});
+		int OldProxUnits = proxBar.getProgress();
+		
+		//save settings as exit
+		solo.clickOnButton("Save as favorite");
+		cp.finish();
+		
+		//load settings
+		cp = (ConfirmationPage) solo.getCurrentActivity();
+		//cp.loadRecentSettings();
+		//boolean vibEquals =  (VibCheck == cp.vibration);
+		//boolean ringEquals = (oldRingtone.equals(cp.dataRingtone));  
+		//boolean proxiEquals = (oldProximity == cp.proximity);
+		//boolean proxUnits = (OldProxUnits == Integer.parseInt(cp.proximityUnit));
+	
+		//check if settings were loaded correctly
+		//assertTrue(vibEquals && ringEquals && proxiEquals && proxUnits);
+	}
 	
 	//test for most recent routes
 	public void test3() throws Throwable{
 		Solo solo = new Solo(getInstrumentation(), getActivity());
 		MainPage mp = (MainPage) solo.getCurrentActivity();
-		mp.getCurrentFocus().focusSearch(0).performClick();
-		//final TextView recentItem = new TextView(mp);
-		//recentItem.performClick();
+		mp.getCurrentFocus();
+		
+		solo.clickOnScreen(solo.getViews().get(2));
+		
 	}
 	
 	//test for major locations
 	public void test4() throws Throwable{
 		Solo solo = new Solo(getInstrumentation(), getActivity());
 		solo.clickOnButton("Major Locations");
+		ArrayList<HashMap<String, String>> locationList = new ArrayList<HashMap<String, String>>();
+		//Intent i = new Intent(view.getContext(), ConfirmationPage.class);
+    	//HashMap<String, String> item = locationList.get(position);
+    	DataFetcher df = new DataFetcher();
+		
 	}
-	
+	*/
 	//test for favorite locations
 	public void test5() throws Throwable{
 		Solo solo = new Solo(getInstrumentation(), getActivity());
 		solo.clickOnButton("Favorites");
-		
+		solo.clickOnScreen(solo.getCurrentListViews().get(2).findFocus());
+		solo.clickOnScreen(solo.getCurrentListViews().get(0));
+		solo.getCurrentListViews().get(0).performClick();
 	}
 	
 	
 }
+
